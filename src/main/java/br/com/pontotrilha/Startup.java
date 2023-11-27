25cf21cf6f268447e5ae20139c68159313458647
Willhan Kreuz <willhan.kreuz@santerra.local>
2023-11-27 12:52:59 -0300

    Criação das rotas de pagamento e ajustes no cadastro de evento

--- src/main/java/br/com/pontotrilha/Startup.java
+++ src/main/java/br/com/pontotrilha/Startup.java
@@ -3,8 +3,17 @@ package br.com.pontotrilha;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.stripe.Stripe;
+
+import jakarta.annotation.PostConstruct;
+
 @SpringBootApplication
 public class Startup {
+
+    /*@PostConstruct
+    public void setup(){
+        Stripe.apiKey = "";
+    }*/
 
     public static void main(String[] args) {
         SpringApplication.run(Startup.class, args);
